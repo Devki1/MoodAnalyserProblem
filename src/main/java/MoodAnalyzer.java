7ae35bcefe84e33a8b2098d920a46c647223ce15
Devki1 <gdev3195@gmail.com>
2020-03-06 18:05:45 +0530

    REFACTORED:Added a function to return happy

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -2,27 +2,25 @@ import javax.swing.*;
 
 public class MoodAnalyzer {
     private String message;
+
     //NO argument parameters constructor
-    MoodAnalyzer()
-    {
+    MoodAnalyzer() {
     }
+
     //Parameters constructor
-    MoodAnalyzer(String message)
-    {
+    MoodAnalyzer(String message) {
         this.message = message;
     }
-    public String MoodAnalyzer()
-    {
-        this.message = message;
-        return analyzeMood();
-    }
+
+
     public String analyzeMood() {
-        if (message.equals("I am a Sad Mood"))
-        {
-            return message = "SAD";
+        if (message.equals("I am a Sad Mood")) {
+            message = "SAD";
+        }
+        else if (message.equals("I am in Happy Mood")) {
+             message = "HAPPY";
         }
         return message;
     }
 }
-
 
