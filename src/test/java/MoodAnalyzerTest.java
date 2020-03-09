cef22ec46755f9c5982fc74e4107929d4542d053
Devki1 <gdev3195@gmail.com>
2020-03-09 14:35:22 +0530

    ADDED:Test case for input as null mood which return happy

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,20 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
-public class MoodAnalyzerTest
-{
-    @Test
-    public void givenMessage_WhenProper_ShouldReturnSadMood()
-    {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am a Sad Mood");
 
-        String  result = moodAnalyzer.analyzeMood();
-     Assert.assertEquals("SAD",result);
-    }
+public class MoodAnalyzerTest {
     @Test
-    public void givenMessage_WhenProper_RespondsHappyMood()
-    {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+    public void givenMessage_WhenProper_ShouldReturnSadMood() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am a Sad Mood");
         String result = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY",result);
+        Assert.assertEquals("SAD", result);
+    }
+
+    @Test
+    public void givenMoodAnalyzer_WhenMessageNotIncludesSad_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String result = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("HAPPY", result);
     }
 }
