60dd462d005ca562e6fdd09cba0867825e8e5d19
Devki1 <gdev3195@gmail.com>
2020-03-12 16:11:11 +0530

    REFACTORED:Added NO_SUCH_METHOD element enum

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,7 +1,7 @@
 public class MoodAnalysisException extends Exception {
     enum ExceptionType {
         ENTERED_NULL, ENTERED_EMPTY,
-        NO_SUCH_CLASS
+        NO_SUCH_CLASS, NO_SUCH_METHOD
     }
 
     ExceptionType type;
@@ -10,7 +10,4 @@ public class MoodAnalysisException extends Exception {
         super(message);
         this.type = type;
     }
-
-
 }
-
