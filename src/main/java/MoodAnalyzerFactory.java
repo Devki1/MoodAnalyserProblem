d45586160a4861308d0d2b3f2f4e918a47d9c775
Devki1 <gdev3195@gmail.com>
2020-03-12 16:14:45 +0530

    REFACTORED:Made constructor to pass wrong parameter

--- src/main/java/MoodAnalyzerFactory.java
+++ src/main/java/MoodAnalyzerFactory.java
@@ -4,14 +4,14 @@ import java.lang.reflect.InvocationTargetException;
 public class MoodAnalyzerFactory {
     public static MoodAnalyzer createMoodAanayzerObject() throws MoodAnalysisException {
         try {
-            Class<?> moodAnalyzer = Class.forName("MoodAnalyzerMood");
+            Class<?> moodAnalyzer = Class.forName("MoodAnalyzer");
             Constructor<?> moodAnalysisConstructor = moodAnalyzer.getConstructor();
             Object moodObject = moodAnalysisConstructor.newInstance();
             return (MoodAnalyzer) moodObject;
         } catch (ClassNotFoundException e) {
-            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "Class not found ");
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "Class not found");
         } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Method not found");
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
