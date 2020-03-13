ecb9c4ac191e58f3d7da82a9f4d3e059f7242379
Devki1 <gdev3195@gmail.com>
2020-03-13 15:37:47 +0530

    REFACTORED:Create a method call invokeMethod

--- src/main/java/MoodAnalyzerFactory.java
+++ src/main/java/MoodAnalyzerFactory.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MoodAnalyzerFactory {
     public static Constructor<?> getConstructor(String className, Class<?>... param) throws MoodAnalysisException {
@@ -14,7 +15,18 @@ public class MoodAnalyzerFactory {
         }
     }
 
-    public static MoodAnalyzer createMoodAnalyserObject(Constructor<?> moodAnalyserConstructor) throws IllegalAccessException, InvocationTargetException, InstantiationException {
-        return (MoodAnalyzer) moodAnalyserConstructor.newInstance();
+    public static MoodAnalyzer createMoodAnalyserObject(Constructor<?> moodAnalyserConstructor, Object... message) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        return (MoodAnalyzer) moodAnalyserConstructor.newInstance(message);
+    }
+
+    public static Object invokeMethod(MoodAnalyzer moodAnalyzerObject, String methodName) throws InvocationTargetException, IllegalAccessException, MoodAnalysisException {
+        Method method = null;
+        try {
+            method = moodAnalyzerObject.getClass().getMethod(methodName);
+            Object result = method.invoke(moodAnalyzerObject);
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Method not found");
+        }
+        return method.invoke(moodAnalyzerObject);
     }
 }
